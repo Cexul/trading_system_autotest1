b94516cdaba9dac874217ce47ad1e4fdb14497e1
xuliang.chen <13703727476@163.com>
2024-06-12 14:26:37 +0800

    yaml读取配置文件

--- common/tools.py
+++ common/tools.py
@@ -4,9 +4,34 @@
 # @Author: xuliang
 
 import datetime
+import os.path
+
 
 def get_now_time():
     return datetime.datetime.now()
 
+def get_project_path():
+    """
+    获取项目的绝对路径
+    :return:
+    """
+    project_name = 'trading_system_autotest1'
+    file_path = os.path.dirname(__file__)
+    # print(file_path)
+    # print(file_path.find(project_name))
+    # print(file_path[:file_path.find(project_name)+len(project_name)])
+    return file_path[:file_path.find(project_name)+len(project_name)]
+
+def sep(path,add_sep_before=False,add_sep_after=False):
+    all_path = os.sep.join(path)
+    if add_sep_before:
+        all_path = os.sep +all_path
+    if add_sep_after:
+        all_path = all_path +os.sep
+    # print(all_path)
+    return all_path
+
 if __name__ == '__main__':
-    print(get_now_time())
+    # print(get_project_path())
+    print(get_project_path())
+    sep(['congif','environment.yaml'],add_sep_after=True)
---
+++ common/yaml_config.py
@@ -0,0 +1,23 @@
+#! /usr/local/bin/python3.10
+# coding=utf-8
+# @Time: 2024/6/12 13:57
+# @Author: xuliang
+
+import yaml
+from common.tools import get_project_path,sep
+
+
+class GetConf(object):
+
+    def __init__(self):
+        with open(get_project_path()+sep(['config','environment.yaml'],add_sep_before=True), 'r',
+                  encoding='utf-8') as env_file:
+            self.env = yaml.load(env_file,Loader=yaml.FullLoader)
+            # print(self.env)
+
+    def get_username_password(self):
+        return self.env['username'],self.env['password']
+
+
+if __name__ == '__main__':
+    print(GetConf().get_username_password())
