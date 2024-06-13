fb8ddfceaf0e9b7a019718b679b6b34befed6341
xuliang.chen <13703727476@163.com>
2024-06-13 11:02:58 +0800

    contains和starts-with定位方法

---
+++ base/HomeBase.py
@@ -0,0 +1,27 @@
+#! /usr/local/bin/python3.10
+# coding=utf-8
+# @Time: 2024/6/13 10:53
+# @Author: xuliang
+
+class HomeBase(object):
+
+    def wallet_switch(self):
+        """
+        首页的钱包开关
+        :return:
+        """
+        return "//span[contains(@class,'switch')]"
+
+    def logo(self):
+        """
+        进入系统后，首页左上角logo
+        :return:
+        """
+        return "//div[contain(text(),'二手')]"
+
+    def welcome(self):
+        """
+        首页欢迎您回来
+        :return:
+        """
+        return "//span[starts-with(text(),'欢迎您回来')]"
