005bed0bd2b9886daf578948fee093e27f08bc9c
xuliang.chen <13703727476@163.com>
2024-06-13 12:50:54 +0800

    等待页面完全加载完成

--- base/ObjectMap.py
+++ base/ObjectMap.py
@@ -5,14 +5,13 @@
 
 
 import time
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.common.exceptions import ElementNotVisibleException
+
+from selenium.common.exceptions import ElementNotVisibleException, WebDriverException
 
 
 class ObjectMap(object):
 
-    def element_get(self, driver,locate_type,locator_expression,timeout=10,must_be_visible=False):
+    def element_get(self, driver, locate_type, locator_expression, timeout=10, must_be_visible=False):
         """
         单个元素获取
         :param driver: 浏览器驱动
@@ -30,7 +29,7 @@ class ObjectMap(object):
 
             # 查找元素
             try:
-                element = driver.find_element(by=locate_type,value=locator_expression)
+                element = driver.find_element(by=locate_type, value=locator_expression)
                 # 如果元素不是必须可见的，就直接返回元素
                 if not must_be_visible:
                     return element
@@ -42,9 +41,37 @@ class ObjectMap(object):
                         raise Exception()
 
             except Exception:
-                now_ms = time.time()*1000
+                now_ms = time.time() * 1000
                 if now_ms >= end_ms:
                     break
                 pass
             time.sleep(0.1)
-        raise ElementNotVisibleException('元素定位失败，定位方式:'+locate_type+'定位表达式:'+locator_expression)
+        raise ElementNotVisibleException('元素定位失败，定位方式:' + locate_type + '定位表达式:' + locator_expression)
+
+    def wait_for_ready_state_complete(self, driver, timeout=30):
+        """
+
+        :param driver: 浏览器驱动
+        :param timeout: 超时时间
+        :return:
+        """
+        # 开始时间
+        start_time = time.time() * 1000
+        # 结束时间
+        end_time = start_time + (timeout * 1000)
+        for x in range(int(timeout * 10)):
+            try:
+                ready_state = driver.execute_scripe('return document.readyState')
+            except WebDriverException:
+                # 如果有driver的错误，执行js失败，就直接跳过
+                time.sleep(0.03)
+                return True
+            if ready_state == 'complete':
+                time.sleep(0.01)
+                return True
+            else:
+                now_ms = time.time() * 1000
+                if now_ms >= end_time:
+                    break
+                time.sleep(0.1)
+        raise Exception('打开网页时，页面元素在%s秒后仍然没有加载完成' % timeout)
