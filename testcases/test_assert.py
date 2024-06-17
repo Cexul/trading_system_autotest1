caae8c41112980a65093d31051c4d5dbeb21d461
xuliang.chen <13703727476@163.com>
2024-06-17 14:50:07 +0800

    pytest_assume插件使用

---
+++ testcases/test_assert.py
@@ -0,0 +1,15 @@
+#! /usr/local/bin/python3.10
+# coding=utf-8
+# @Time: 2024/6/17 14:38
+# @Author: xuliang
+
+import pytest
+from pytest_assume.plugin import assume
+
+class TestAssert(object):
+    def test_assert(self):
+        with assume: assert 'as' in 'wer'
+
+        pytest.assume(1+1==3)
+        assert 1+1==2
+        print('over')
