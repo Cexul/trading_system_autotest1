0f968bbe070b4875f1659cc416ac1f0caef259e2
xuliang.chen <13703727476@163.com>
2024-06-17 12:22:44 +0800

    测试用例中商品和tabs的参数化

--- testcases/test_add_goods.py
+++ testcases/test_add_goods.py
@@ -11,10 +11,32 @@ from page.LoginPage import LoginPage
 from page.LeftMenuPage import LeftMenuPage
 from page.GoodsPage import GoodsPage
 
+goods_info_list = [
+    {
+        'goods_title': '新增商品测试jay1',
+        'goods_detail': '新增商品测试jay详情1',
+        'goods_num': 2,
+        'goods_pic_list': ['img1.jpg'],
+        'goods_price': 233,
+        'goods_status': '上架',
+        'bottom_button_name': '提交'
+    },
+    {
+        'goods_title': '新增商品测试jay2',
+        'goods_detail': '新增商品测试jay详情2',
+        'goods_num': 2,
+        'goods_pic_list': ['img1.jpg'],
+        'goods_price': 333,
+        'goods_status': '上架',
+        'bottom_button_name': '提交'
+    },
+]
+
 
 class TestAddGoods(object):
 
-    def test_add_goods_001(self, driver):
+    @pytest.mark.parametrize('goods_info', goods_info_list)
+    def test_add_goods_001(self, driver, goods_info):
         # driver = DriverConfig().driver_config()
         LoginPage().login(driver, 'jay')
         LeftMenuPage().click_level_one_menu(driver, '产品')
@@ -23,12 +45,12 @@ class TestAddGoods(object):
         time.sleep(1)
         GoodsPage().add_new_goods(
             driver,
-            goods_title='新增商品测试jay',
-            goods_detail='新增商品测试jay详情',
-            goods_num=2,
-            goods_pic_list=['img1.jpg'],
-            goods_price=233,
-            goods_status='上架',
-            bottom_button_name='提交'
+            goods_title=goods_info['goods_title'],
+            goods_detail=goods_info['goods_detail'],
+            goods_num=goods_info['goods_num'],
+            goods_pic_list=goods_info['goods_pic_list'],
+            goods_price=goods_info['goods_price'],
+            goods_status=goods_info['goods_status'],
+            bottom_button_name=goods_info['bottom_button_name']
         )
         time.sleep(3)
